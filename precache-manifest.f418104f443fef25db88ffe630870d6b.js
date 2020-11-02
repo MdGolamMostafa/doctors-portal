40b4d4ba5a714349412fa40d85966a2be1e72c53
MdGolamMostafa <shakilhero11@gmail.com>
2020-11-02 14:09:54 +0600

    Updates

---
+++ precache-manifest.f418104f443fef25db88ffe630870d6b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f66dd7e2ba2e95151f5a20b4b00895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28b55afb044f7df01322",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "4492e4df1ec09db41836",
+    "url": "/static/css/main.9a3f7c55.chunk.css"
+  },
+  {
+    "revision": "28b55afb044f7df01322",
+    "url": "/static/js/2.80ce9815.chunk.js"
+  },
+  {
+    "revision": "1122ba7238d0def00b3b36d69f1c11b7",
+    "url": "/static/js/2.80ce9815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4492e4df1ec09db41836",
+    "url": "/static/js/main.30ad2ed1.chunk.js"
+  },
+  {
+    "revision": "3f96878e8bff55d04591",
+    "url": "/static/js/runtime-main.85f32fef.js"
+  },
+  {
+    "revision": "bc07148151272782483df15032e26913",
+    "url": "/static/media/DentalCareImg.bc071481.png"
+  },
+  {
+    "revision": "bbf64a8f9a41560209ffebaaaa0fa584",
+    "url": "/static/media/aliza.bbf64a8f.png"
+  },
+  {
+    "revision": "c2cbe1c887fb9f052bb4fae80eda4dd9",
+    "url": "/static/media/appointmentBg.c2cbe1c8.png"
+  },
+  {
+    "revision": "f112c368027f8066055d5f4782a055ae",
+    "url": "/static/media/chair.f112c368.png"
+  },
+  {
+    "revision": "f843a8b8971103b70066e625d49ffc01",
+    "url": "/static/media/chairBg.f843a8b8.png"
+  },
+  {
+    "revision": "eb24d43983f5930ba697e54c8f1f9781",
+    "url": "/static/media/doctor-sm.eb24d439.png"
+  },
+  {
+    "revision": "b7d9c9e6d5bb6f861409dd294e663724",
+    "url": "/static/media/doctor.b7d9c9e6.png"
+  },
+  {
+    "revision": "e1f2c3c6d4873840abfd699d5eaef615",
+    "url": "/static/media/ema.e1f2c3c6.png"
+  },
+  {
+    "revision": "7e5aec06294edf5718bd75237dac47f7",
+    "url": "/static/media/footer-bg.7e5aec06.png"
+  },
+  {
+    "revision": "6de7b101b2556f0f7a8c62a18e329e92",
+    "url": "/static/media/wilson.6de7b101.png"
+  }
+]);
